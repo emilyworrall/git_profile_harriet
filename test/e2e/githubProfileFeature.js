46d820ca1e550d08ef7f83d55cc6186dce873e73
Emily Worrall <worrall.emily@gmail.com>
2015-10-23 11:53:28 +0100

    Started mocking HTTP ajax request for protractor

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,38 +1,45 @@
+var mock = require('protractor-http-mock');
+
 describe('GitHub profile finder', function() {
 
-    var searchBox = element(by.model('searchCtrl.searchTerm'))
-    var searchButton = element(by.className('btn'))
+  beforeEach(function() {
+    mock(['githubUserSearch.js']);
+    browser.get('http://localhost:8080');
+  });
 
-    beforeEach(function(){
-      browser.get('http://localhost:8080');
+  afterEach(function() {
+    mock.teardown();
+  });
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  it('has a title', function() {
+     expect(browser.getTitle()).toEqual('Github user search');
+   });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike01');
+  });
+
+  it('finds the last Spike', function() {
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('spikelynch');
+  });
+
+  it('count the number of Spikes', function() {
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    element.all(by.repeater('user in searchCtrl.searchResult.items')).then(function(items) {
+      expect(items.length).toBe(30);
     });
-
-    it('has a title', function() {
-       expect(browser.getTitle()).toEqual('Github user search');
-     });
-
-    it('finds profiles', function() {
-      searchBox.sendKeys('spike01');
-      searchButton.click();
-
-      var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-      expect(profiles.get(0).getText()).toEqual('spike01');
-    });
-
-    it('finds the last Spike', function() {
-      searchBox.sendKeys('spike');
-      searchButton.click();
-
-      var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-      expect(profiles.last().getText()).toEqual('spikelynch');
-    });
-
-    it('count the number of Spikes', function() {
-      searchBox.sendKeys('spike');
-      searchButton.click();
-
-      element.all(by.repeater('user in searchCtrl.searchResult.items')).then(function(items) {
-        expect(items.length).toBe(30);
-      });
-    });
+  });
 });
---
+++ test/e2e/mocks/githubUserSearch.js
@@ -0,0 +1,54 @@
+module.exports = [
+  {
+    request: {
+      path: 'https://api.github.com/search/users',
+      method: 'GET'
+    },
+    response: {
+      data: {
+        total_count: 8,
+        incomplete_results: false,
+        items: [
+        {
+        login: "ptolemybarnes",
+        id: 9342866,
+        avatar_url: "https://avatars.githubusercontent.com/u/9342866?v=3",
+        gravatar_id: "",
+        url: "https://api.github.com/users/ptolemybarnes",
+        html_url: "https://github.com/ptolemybarnes",
+        followers_url: "https://api.github.com/users/ptolemybarnes/followers",
+        following_url: "https://api.github.com/users/ptolemybarnes/following{/other_user}",
+        gists_url: "https://api.github.com/users/ptolemybarnes/gists{/gist_id}",
+        starred_url: "https://api.github.com/users/ptolemybarnes/starred{/owner}{/repo}",
+        subscriptions_url: "https://api.github.com/users/ptolemybarnes/subscriptions",
+        organizations_url: "https://api.github.com/users/ptolemybarnes/orgs",
+        repos_url: "https://api.github.com/users/ptolemybarnes/repos",
+        events_url: "https://api.github.com/users/ptolemybarnes/events{/privacy}",
+        received_events_url: "https://api.github.com/users/ptolemybarnes/received_events",
+        type: "User",
+        site_admin: false,
+        score: 75.690094
+      },
+      {
+        login: "ptolemyrulz",
+        id: 588827,
+        avatar_url: "https://avatars.githubusercontent.com/u/588827?v=3",
+        gravatar_id: "",
+        url: "https://api.github.com/users/ptolemyrulz",
+        html_url: "https://github.com/ptolemyrulz",
+        followers_url: "https://api.github.com/users/ptolemyrulz/followers",
+        following_url: "https://api.github.com/users/ptolemyrulz/following{/other_user}",
+        gists_url: "https://api.github.com/users/ptolemyrulz/gists{/gist_id}",
+        starred_url: "https://api.github.com/users/ptolemyrulz/starred{/owner}{/repo}",
+        subscriptions_url: "https://api.github.com/users/ptolemyrulz/subscriptions",
+        organizations_url: "https://api.github.com/users/ptolemyrulz/orgs",
+        repos_url: "https://api.github.com/users/ptolemyrulz/repos",
+        events_url: "https://api.github.com/users/ptolemyrulz/events{/privacy}",
+        received_events_url: "https://api.github.com/users/ptolemyrulz/received_events",
+        type: "User",
+        site_admin: false,
+        score: 18.009634
+      }
+    }
+  }
+]
